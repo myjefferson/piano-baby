1fc8233f9dcbe00d61cd5c047be440a2f11ddacd
MyJefferson <jcsjeffrey@gmail.com>
2022-07-19 23:41:51 -0300

    feature: style piano

--- src/pages/style/styles.ts
+++ src/pages/style/styles.ts
@@ -14,4 +14,44 @@ export const Main = styled.main`
 
 export const Piano = styled.div`
     width: max-content;
+
+    button{
+        cursor: pointer;
+        padding: 25px 10px 20px 10px;
+        margin: 0 3px 0 3px;
+        border-radius: 10px;
+        border: none;
+    }
+
+    button:nth-child(1){
+        background: red;
+    }
+
+    button:nth-child(2){
+        background: green;
+    }
+
+    button:nth-child(3){
+        background: blue;
+    }
+
+    button:nth-child(4){
+        background: red;
+    }
+
+    button:nth-child(5){
+        background: red;
+    }
+
+    button:nth-child(6){
+        background: red;
+    }
+    
+    button:nth-child(7){
+        background: red;
+    }
+
+    button:nth-child(8){
+        background: red;
+    }
 `;
