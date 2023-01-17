c27c0fd7d6698b830207bccfcf4a6b50471a410c
Jefferson Carvalho <68786776+myjefferson@users.noreply.github.com>
2023-01-17 12:08:08 -0300

    Add files via upload

---
+++ src/pages/Footer.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import { FooterStyle } from './style/Footer'
+
+const Footer = () =>{
+    return(
+        <FooterStyle>
+            Desenvolvido com 💙 por <a href="https://github.com/myjefferson" target="_blank" rel="noreferrer"> Jefferson Carvalho </a>
+            <br/>
+            Para: Joe Yohan Carvalho (meu irmãozinho)
+        </FooterStyle>
+    )
+    
+}
+
+export default Footer
