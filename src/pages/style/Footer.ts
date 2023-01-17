815566e852f049166324617cfe3e385baafe12d5
Jefferson Carvalho <68786776+myjefferson@users.noreply.github.com>
2023-01-17 12:08:37 -0300

    Add files via upload

---
+++ src/pages/style/Footer.ts
@@ -0,0 +1,21 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export const FooterStyle = styled.div`
+    width: 100%;
+    padding: 3px;
+    position: fixed;
+    z-index: 1000;
+    left: 0;
+    bottom: 0;
+
+    font-size: 14px;
+    text-align: center;
+    font-family: Arial;
+    background: rgba(255,255,255,0.85);
+
+    a{
+        color: #0073B9;
+        font-stlyle: none;
+    }
+`;
